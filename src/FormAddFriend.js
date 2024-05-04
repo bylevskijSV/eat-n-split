038467b076fc4b0858844b847dba55248c7a50de
Sergei Bylevskiy <sergeibylewskii@gmail.com>
2024-05-04 16:06:06 +0300

    add ability to add friends to list

--- src/FormAddFriend.js
+++ src/FormAddFriend.js
@@ -1,13 +1,46 @@
 import Button from "./Button";
+import { useState } from "react";
 
-export default function FormAddFriend() {
+const DEFAULT_IMAGE_URL = "https://i.pravatar.cc/48";
+
+export default function FormAddFriend({ onAddFriend }) {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState(DEFAULT_IMAGE_URL);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!name || !image) return;
+
+    const id = crypto.randomUUID();
+    const newFriend = {
+      id,
+      name,
+      image: `${image}?=${id}`,
+      balance: 0,
+    };
+
+    onAddFriend(newFriend);
+
+    setName("");
+    setImage(DEFAULT_IMAGE_URL);
+  }
+
   return (
-    <form className="form-add-friend">
+    <form className="form-add-friend" onSubmit={handleSubmit}>
       <label>🤰 Friend name</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
 
       <label>🖼 Image url</label>
-      <input type="text"/>
+      <input
+        type="text"
+        value={image}
+        onChange={(e) => setImage(e.target.value)}
+      />
 
       <Button>Add</Button>
     </form>
