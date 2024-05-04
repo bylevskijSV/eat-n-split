a3a10e180ddc50a9603cf6218bea5eace81ca5e9
Sergei Bylevskiy <sergeibylewskii@gmail.com>
2024-05-04 13:58:32 +0300

    add togling to FormAddFriend

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import FriendList from "./FriendsList";
 import FormAddFriend from "./FormAddFriend";
 import Button from "./Button";
 import FormSplitBill from "./FormSplitBill";
+import { useState } from "react";
 
 function App() {
+  const [showAddFriend, setShowAddFriend] = useState(false);
+
+  function handleShowAddFriend() {
+    setShowAddFriend((show) => !show);
+  }
+
   return (
     <div className="app">
       <div className="sidebar">
         <FriendList />
-        <FormAddFriend />
-        <Button>Add friend</Button>
+        {showAddFriend && <FormAddFriend />}
+        <Button onClick={handleShowAddFriend}>
+          {showAddFriend ? "Close" : "Add friend"}
+        </Button>
       </div>
 
       <FormSplitBill />
